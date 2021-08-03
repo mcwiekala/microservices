4006f19f111326e699949dd8e9ea938c04872897
Michal Cwiekala <80964094+mcwiekala@users.noreply.github.com>
2021-08-03 13:34:31 +0200

    added elastic index client

---
+++ elastic/elastic-index-client/src/main/java/pl/microservices/demo/elastic/index/client/service/ElasticIndexClient.java
@@ -0,0 +1,12 @@
+package pl.microservices.demo.elastic.index.client.service;
+
+import pl.microservices.demo.elastic.model.index.IndexModel;
+
+import java.util.List;
+
+public interface ElasticIndexClient<T extends IndexModel> {
+
+    List<String> save(List<T> documents);
+
+
+}
