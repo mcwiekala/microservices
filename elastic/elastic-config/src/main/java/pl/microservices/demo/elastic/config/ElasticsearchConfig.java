9b9f66b465f0f5d0cfc644ad51d921f4985fc08a
Michal Cwiekala <80964094+mcwiekala@users.noreply.github.com>
2021-08-03 12:44:46 +0200

    added elastic config

---
+++ elastic/elastic-config/src/main/java/pl/microservices/demo/elastic/config/ElasticsearchConfig.java
@@ -0,0 +1,42 @@
+package pl.microservices.demo.elastic.config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+import pl.microservices.demo.config.ElasticConfigData;
+
+import java.util.Objects;
+
+@Configuration
+public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
+
+    private final ElasticConfigData elasticConfigData;
+
+    public ElasticsearchConfig(ElasticConfigData elasticConfigData) {
+        this.elasticConfigData = elasticConfigData;
+    }
+
+    @Override
+    @Bean
+    public RestHighLevelClient elasticsearchClient() {
+        UriComponents serverUri = UriComponentsBuilder.fromHttpUrl(elasticConfigData.getConnectionUrl()).build();
+
+        HttpHost httpHost = new HttpHost(
+                Objects.requireNonNull(serverUri.getHost()),
+                serverUri.getPort(),
+                serverUri.getScheme());
+
+        RestClientBuilder restClientBuilder = RestClient.builder(httpHost)
+                .setRequestConfigCallback( builder -> builder
+                        .setConnectTimeout(elasticConfigData.getConnectionTimeoutMs())
+                        .setSocketTimeout(elasticConfigData.getSocketTimeoutMs()));
+
+        return new RestHighLevelClient(restClientBuilder);
+    }
+}
