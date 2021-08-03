a2fd61b083043d10f9e65a2ef5d7b6334ca61ed2
Michal Cwiekala <80964094+mcwiekala@users.noreply.github.com>
2021-08-03 12:22:00 +0200

    added elastic index model

---
+++ app-config-data/src/main/java/pl/microservices/demo/config/ElasticConfigData.java
@@ -0,0 +1,19 @@
+package pl.microservices.demo.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "elastic-config")
+public class ElasticConfigData {
+
+    private String indexName;
+    private String connectionUrl;
+    private Integer connectionTimeoutMs;
+    private Integer socketTimeoutMs;
+
+}
---
+++ elastic/elastic-model/src/main/java/pl/microservices/demo/elastic/model/index/IndexModel.java
@@ -0,0 +1,7 @@
+package pl.microservices.demo.elastic.model.index;
+
+public interface IndexModel {
+
+    String getId();
+
+}
