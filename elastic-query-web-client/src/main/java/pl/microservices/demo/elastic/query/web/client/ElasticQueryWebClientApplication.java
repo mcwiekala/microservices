5366ee07bd40504690222d5e75a2b6fa68654697
Michal Cwiekala <80964094+mcwiekala@users.noreply.github.com>
2021-08-04 14:57:20 +0200

    added elastic-query-web-client module

---
+++ elastic-query-web-client/src/main/java/pl/microservices/demo/elastic/query/web/client/ElasticQueryWebClientApplication.java
@@ -0,0 +1,16 @@
+package pl.microservices.demo.elastic.query.web.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "pl.microservices.demo")
+public class ElasticQueryWebClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticQueryWebClientApplication.class, args);
+    }
+
+
+}
